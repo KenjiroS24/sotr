0706465162fb7a64fbd27419baf5bf30b3502175
Artem Titov <106061547+KenjiroS24@users.noreply.github.com>
2022-05-25 17:47:43 +0300

    Update sotr_game.get_health.sql

--- sotr_game.get_health.sql
+++ sotr_game.get_health.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION sotr_game.get_health()
  LANGUAGE plpgsql
 AS $function$
 declare 
+p_res 		text;
 p_in_cnt	int4;
 p_effect	int4;
 p_max_hp	int4;
@@ -13,29 +14,25 @@ begin
 	--Проверка наличия инвентаря с heal
 	select gi.in_cnt into p_in_cnt 
 		from sotr_game.g_inventory as gi 
-		join sotr_settings.items as i on i.i_id = gi.in_items_id 
 	where in_items_id = 1;
 	if not found then 
 		return  'У Вас нет Трав в инвентаре';
 	end if;
 
-	--Проверка количества Травы в запасе
-	if p_in_cnt <= 0 then 
-		return 'У Вас закончилась Трава';
-	else 
-		select (i.effect ->> 'num')::int4 into p_effect
-			from sotr_settings.items i
-		where i_id = 1;
-	end if;
+	--Назначение очков хилла
+	select (i.effect ->> 'num')::int4 into p_effect
+		from sotr_settings.items i
+	where i_id = 1;
 	
 	--Проверка предела ХП
 	select hsl.heal_points as hhp, gh.h_heal_points as ghhp into p_max_hp, p_now_hp
 		from sotr_settings.hero_state_lvl hsl 
 		join sotr_game.g_hero gh on hsl.lvl_id = gh.h_lvl;
-	
+
+	--Если текущий уровень здоровья равен максимальному, ничего не делать
 	if p_now_hp = p_max_hp then 
-		return 'Персонаж не нуждается в лечении.';
-	
+		p_res = 'Персонаж не нуждается в лечении.';
+	--Если после прибавления очков ХП будет достигнут или преодолен предел ХП, уровень ХП обновится на свой максимум и не выше
 	elseif p_now_hp + p_effect >= p_max_hp then 
 		update sotr_game.g_hero 
 			set h_heal_points = p_max_hp
@@ -46,31 +43,27 @@ begin
 		where in_items_id = 1
 		returning in_cnt into p_in_cnt;
 	
-		if p_in_cnt <= 0 then 
-			delete from sotr_game.g_inventory
-			where in_items_id = 1;
-		end if;
-	
-		return 'ХП повысилось до максимального значения. Ваше ХП составляет ' || p_max_hp;
-	
-	else update sotr_game.g_hero 
+		p_res = 'ХП повысилось до максимального значения. Ваше ХП составляет [' || p_max_hp::text || '/' || p_max_hp::text || ']';
+	else 
+		update sotr_game.g_hero 
 			set h_heal_points = h_heal_points + p_effect
-		where h_id = 1;
+		where h_id = 1
+		returning h_heal_points into p_now_hp;
 	
 		update sotr_game.g_inventory 
 			set in_cnt = in_cnt - 1
 		where in_items_id = 1
 		returning in_cnt into p_in_cnt;
 		
-		if p_in_cnt <= 0 then 
-			delete from sotr_game.g_inventory
-			where in_items_id = 1;
-		end if;
-	
+		p_res = 'Ваше ХП повысилось на [' || p_effect || ']. Ваше ХП составляет [' || p_now_hp::text || '/' || p_max_hp::text || ']';
 	end if;
 
-	return 'Ваше ХП повысилось на ' || p_effect;
+	if p_in_cnt <= 0 then 
+		delete from sotr_game.g_inventory
+		where in_items_id = 1;
+	end if;
 
+	return p_res;
 end;
 $function$
 ;
